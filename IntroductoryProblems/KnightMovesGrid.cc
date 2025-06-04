517f35e0ff22e57a381c1191915858c9c68233a3
hubotao <lnfromnorth@qq.com>
2025-06-04 15:01:02 +0800

    fix: KnightMovesGrid maybe i am right at the begining

--- IntroductoryProblems/KnightMovesGrid.cc
+++ IntroductoryProblems/KnightMovesGrid.cc
@@ -4,42 +4,34 @@ using namespace std;
 
 int xi[4] = {-1, -2, -2, -1};
 int yi[4] = {-2, -1, 1, 2};
-int init[6] = {0, 3, 4, 2, 1, 4};
+int init[21] = {0, 3, 4, 2, 1, 4, 3, 2, 3, 2, 2, 3, 2, 3, 4, 3, 4, 3, 4, 3, 4};
 
 #define IDX(x, y) ((x) * ((x) - 1) / 2 + (y) - 1)
 
 int find(int *output, int x, int y) {
-    int max = 0;
-    int count = 0;
-    int dcount = 0;
+    int min = 9999999;
     for (int i = 0; i < 4; i++) {
         int xx = x + xi[i];
         int yy = y + yi[i];
         if (xx > 0 && yy > 0 && yy <= xx) {
-            count++;
-            if (output[IDX(xx, yy) != max])
-                dcount++;
-
-            if (output[IDX(xx, yy)] > max) {
-                max = output[IDX(xx, yy)];
+            if (output[IDX(xx, yy)] < min) {
+                min = output[IDX(xx, yy)];
             }
         }
     }
-    if (x == y || (count > 1 && dcount == 1))
-        return max + 1;
-    else
-        return max - 1;
+    return min + 1;
 }
 
 int main() {
     int num;
     cin >> num;
     int total = num * (num + 1) / 2;
-    int *output = (int *)malloc(total * sizeof(int));
+    int add = ((total - 21) > 0) ? total - 21 : 0;
+    int *output = (int *)malloc((add + 21) * sizeof(int));
     memset(output, 0, (1 + num) / 2 * num * sizeof(int));
-    memcpy(output, init, 6 * sizeof(int));
-    if (num > 3) {
-        for (int x = 4; x <= num; x++) {
+    memcpy(output, init, 21 * sizeof(int));
+    if (num > 6) {
+        for (int x = 7; x <= num; x++) {
             for (int y = 1; y <= x; y++) {
                 output[IDX(x, y)] = find(output, x, y);
             }
@@ -48,9 +40,9 @@ int main() {
     for (int i = 0; i < num; i++) {
         for (int j = 0; j < num; j++) {
             if (i <= j)
-                cout << output[IDX(j + 1, i + 1)];
+                cout << output[IDX(j + 1, i + 1)] << " ";
             else
-                cout << " ";
+                cout << output[IDX(i + 1, j + 1)] << " ";
         }
         cout << endl;
     }
